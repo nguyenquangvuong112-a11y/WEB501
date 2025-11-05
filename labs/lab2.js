17f2d13cd5d97a829245c0326f0bab8bbd2239c7
vuong <nguyenquangvuong112@gmail.com>
2025-11-05 15:28:46 +0700

    d

--- labs/lab2.js
+++ labs/lab2.js
@@ -1,1 +1,6 @@
-gdfgdfg;
+// bai 1
+for (let i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 100);
+}
---
+++ labs/lab3.js
@@ -0,0 +1,9 @@
+// bai 2
+
+const student = {
+  name: "Thuy Tien",
+  age: "20",
+};
+student.grade = "A";
+student.age = "21";
+console.log(student);
