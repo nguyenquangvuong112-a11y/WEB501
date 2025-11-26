43e70c44212fceb36340c90465827990b0977f5b
vuong <nguyenquangvuong112@gmail.com>
2025-11-26 14:17:08 +0700

    lab5

--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -1,66 +1,55 @@
-import Button from '../components/Button'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Footer from '../components/Footer'
 import Header from '../components/Header'
 import Heading from '../components/Heading'
-function App() {
+import TourCard from '../components/TourCard'
+
+function Home() {
   // mock data
-  const tours = [
-    {
-      id: 1,
-      title: 'Tour Du Lịch Cuba',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-    },
-    {
-      id: 2,
-      title: 'Tour Du Lịch Hoa Ky',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/04/Tour-Bo-Dong-My-10N9D-destination-new-york-01-1-1200x800.jpg',
-    },
-    {
-      id: 3,
-      title: 'Tour Du Lịch Nhat Ban ',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
-    },
-    {
-      id: 4,
-      title: 'Tour Du Lịch Nhat Ban ',
-      image:
-        'https://vtourist.com.vn/wp-content/uploads/2024/05/kinh-nghiem-du-lich-sydney-1200x800.jpg',
-    },
-  ]
-  
+  const [tours, setTours] = useState([])
+
+  useEffect(() => {
+    console.log('Chạy 1 lần khi mount')
+    const getTours = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:3001/tours')
+        setTours(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    getTours()
+  }, [])
+
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <Header />
       <main>
-        {/* <h1 className="text-3xl text-blue-600 text-center font-bold border-b-2 border-b-orange-400 uppercase">
-          TOUR NỘI ĐỊA
-        </h1> */}
-          <Heading title="TOUR NỘI ĐỊA"/>
+        <Heading title="Tour Nội Địa" />
         <p className="text-xl font-medium my-2 px-2">
           Các chuyến đi đồng hành cùng chúng tôi là khoảnh khắc đặc biêt, luôn
           sẵn sàng tạo ra những trải nghiệm độc đáo và không quên cho du khách,
           giúp mang đến những chuyến hành trình tuyệt vời.
         </p>
-        <div className="flex gap-2">
+        <div className="flex gap-2 items-center justify-center my-4">
           {tours.map(tour => (
-            <div key={tour.id}>
-              <img src={tour.image} width={500} alt="" height={300} />
-              <h2 className="text-3xl font-bold text-orange-300">
-                {tour.title}
-              </h2>
-               <Button label="Xem Thêm"/>
-               <Button label="Đặt tour"/>
-            </div>
-            
+            <TourCard
+              key={tour.id}
+              id={tour.id}
+              name={tour.name}
+              image={tour.image}
+              destination={tour.destination}
+              price={tour.price}
+            />
           ))}
         </div>
+        <Heading title="Tour Quoc Te" />
       </main>
+
       <Footer />
     </div>
   )
 }
 
-export default App
+export default Home
